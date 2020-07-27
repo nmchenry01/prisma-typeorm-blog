ac5b681f63420811f3d3c7b7c5fa036a66edc9e2
Nicholas McHenry <nicholas.mchenry@levvel.io>
2020-07-27 16:56:31 -0400

    update schemas

--- prismaDemo/src/index.ts
+++ prismaDemo/src/index.ts
@@ -1,5 +1,81 @@
-const main = () => {
-  console.log('Hello world');
+import { PrismaClient, CompanyCreateInput } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const createCompany = async (input: CompanyCreateInput) => {
+  await prisma.company.create({ data: input });
 };
 
-main();
+const findAllCompanies = async () => {
+  return prisma.company.findMany({
+    // Include relations
+    include: {
+      products: true,
+    },
+  });
+};
+
+const findAllProducts = async () => {
+  return prisma.product.findMany({
+    include: {
+      customers: true,
+    },
+  });
+};
+
+const findAllCustomers = async () => {
+  return prisma.customer.findMany({
+    include: {
+      products: true,
+    },
+  });
+};
+
+// const createCustomer = async (input: CreateCustomer)
+
+const main = async (): Promise<void> => {
+  // Create company without products
+  const companyCreateInput: CompanyCreateInput = {
+    name: 'Acme',
+    products: {
+      create: {
+        name: 'Widget',
+        description: 'A type of product',
+      },
+    },
+  };
+  await createCompany(companyCreateInput);
+  // Get all companies
+  const companies = await findAllCompanies();
+  console.dir(companies, { depth: null });
+
+  // Get all products
+  const products = await findAllProducts();
+  console.dir(products, { depth: null });
+
+  // Create a customer and link to product
+  await prisma.customer.create({
+    data: {
+      username: 'admin',
+      email: 'admin@gmail.com',
+      products: {
+        connect: {
+          id: 1,
+        },
+      },
+    },
+  });
+
+  const customers = await findAllCustomers();
+  console.dir(customers, { depth: null });
+};
+
+main()
+  .catch((error) => {
+    // eslint-disable-next-line no-console
+    console.error(error.stack);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.disconnect();
+  });
--- schema.sql
+++ schema.sql
@@ -1,13 +1,26 @@
-CREATE TABLE "public"."Company" (
+CREATE TABLE "Company" (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255),
+  name VARCHAR(255) NOT NULL UNIQUE,
   "createdAt" TIMESTAMP NOT NULL DEFAULT now()
 );
-CREATE TABLE "public"."Product" (
+CREATE TABLE "Product" (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255),
+  name VARCHAR(255) NOT NULL UNIQUE,
   description VARCHAR(255),
   "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
+  "companyId" INTEGER NOT NULL,
+  FOREIGN KEY ("companyId") REFERENCES "Company"(id)
+);
+CREATE TABLE "Customer" (
+  id SERIAL PRIMARY KEY NOT NULL,
+  username VARCHAR(255) NOT NULL UNIQUE,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  "createdAt" TIMESTAMP NOT NULL DEFAULT now()
+);
+CREATE TABLE "ProductToCustomer" (
   "productId" INTEGER NOT NULL,
-  FOREIGN KEY ("productId") REFERENCES "public"."Company"(id)
+  "customerId" INTEGER NOT NULL,
+  PRIMARY KEY ("productId", "customerId"),
+  FOREIGN KEY ("productId") REFERENCES "Product"(id) ON UPDATE CASCADE,
+  FOREIGN KEY ("customerId") REFERENCES "Customer"(id) ON UPDATE CASCADE
 );
